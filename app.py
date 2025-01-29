6ffd27ff8fa5c071a7cf4915531068e9048a26c9
Stuti Nagaich <168558692+Stuti0711@users.noreply.github.com>
2025-01-29 08:50:33 -0800

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def get_response(query):
                 "content": query
             }
         ],
-        model = "llama-3.1-70b-versatile",
+        model = "llama-3.3-70b-versatile",
         temperature=0.5,
         max_tokens=1024,
         top_p=1,
